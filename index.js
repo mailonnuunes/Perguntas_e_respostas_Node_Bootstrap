e90d43c1cba387253b018ee3fc3d988b6723a923
vihalbertino <kellialbertino@gmail.com>
2023-04-09 13:21:58 -0300

    enviado

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.get("/perguntar", function(req,res){
+
+res.render("index")
+})
+app.get("/perguntar",(req, res) =>{
+    res.render("perguntar")
+})
+
+app.listen(8080,()=>{
+    console.log("o site esta rodando!")
+})
